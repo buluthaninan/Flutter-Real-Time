27e926843acd049ff5d4c53746686c895f9e127b
pr0metheus00 <buluthaninan@gmail.com>
2021-08-10 17:27:29 +0300

    basic set up for app.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
-asdsad
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text("Home Screen")),
+    );
+  }
+}
