31d943849bf6e04597e7b1a8fc563fca8abf1aad
pr0metheus00 <buluthaninan@gmail.com>
2021-08-16 16:24:38 +0300

    Project Finished.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -51,7 +51,7 @@ class _HomeState extends State<Home> {
                     },
                     icon: Icon(
                       Icons.edit_location,
-                      color: Colors.grey[301],
+                      color: Colors.grey[300],
                     ),
 
                     label: Text(
